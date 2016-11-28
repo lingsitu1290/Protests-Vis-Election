ed5d1325c9eac590767c381e2d314d846fce2913
Ling <lingsitu1290@gmail.com>
2016-11-28 03:56:52 +0000

    Commented out flask debugger

--- server.py
+++ server.py
@@ -216,7 +216,7 @@ if __name__ == "__main__":
 
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
 
     connect_to_db(app, os.environ.get("DATABASE_URL"))
 
